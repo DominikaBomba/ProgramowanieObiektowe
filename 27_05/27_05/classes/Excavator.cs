e021b1b35cd9df42d0ae086cb8dde5a66e396956
Dominika Bomba <d.bomba806@gmail.com>
2024-06-03 19:35:25 +0200

    Dziedziczenie klas

---
+++ 27_05/27_05/classes/Excavator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_05.classes
+{
+    internal class Excavator : Machine
+    {
+        public int DigDepth { get; set; }
+        public Excavator(string name, int digdepth) : base(name)
+        {
+            DigDepth = digdepth;
+        }
+        public override void Start()
+        {
+            Console.WriteLine($"{Name} ropoczyna kopanie");
+     
+        }
+        public override void Stop()
+        {
+            Console.WriteLine($"{Nam} kończy prace");
+        }
+    }
+}
---
+++ 27_05/27_05/classes/MachineTypeManeger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_05.classes
+{
+    internal class MachineTypeManeger
+    {
+        private List<string> machineType = new List<string>();
+
+        public MachineTypeManeger() {
+            machineType.Add("Koparka");
+            machineType.Add("Dźwig");
+            machineType.Add("Spychacz");
+            machineType.Add("Ładowarka");
+        
+        }
+        public void AddMachineType(string type)
+        {
+            if(!machineType.Contains(type))
+            {
+                machineType.Add(type);
+                Console.WriteLine($"Typ maszyny {type} został dodany");
+            }
+            else
+            {
+                Console.WriteLine($"Typ maszyny {type} już istnieje");
+            }
+        }
+        public void DisplayMachineType()
+        {
+            foreach (string maszyny in machineType)
+            {
+             
+              Console.Write($"{machineType}, ");
+            }
+        }
+        public bool IsValidType(string type)
+        {
+            return machineType.Contains(type);
+        }
+
+
+    }
+}
